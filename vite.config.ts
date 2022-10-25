1d29dcd467b2842c732dab2b9a17a0ae573feebc
OceanPresent <oceanpresent@163.com>
2022-10-25 20:09:07 +0800

    config: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 /// <reference types="vitest" />
 import * as path from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  plugins: [dts()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: 'piniaRouter',
-            fileName: (format: string) => `pinia-router.${format}.js`,
-        },
-    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: (format: string) => `index.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    target: 'esnext',
+  },
 })
